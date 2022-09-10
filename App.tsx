5fea68ee954b42b33aa60b98d4de1d04c37d2bb3
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-09-10 16:39:17 +0530

    rn init

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>{'React Native App'}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
