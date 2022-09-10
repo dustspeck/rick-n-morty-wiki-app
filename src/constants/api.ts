f27147d64a2d64c499e8553c6342d9ec48372af2
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-09-11 00:22:06 +0530

    add axios & config

---
+++ src/constants/api.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://rickandmortyapi.com/api';
+export const CHARACTERS_ROUTE = `${BASE_URL}/character`;
+export const LOCATIONS_ROUTE = `${BASE_URL}/location`;
+export const EPISODES_ROUTE = `${BASE_URL}/episode`;
---
+++ src/services/api/config.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import {BASE_URL} from '../../constants/api';
+
+export const apiClient = axios.create({baseURL: BASE_URL});
